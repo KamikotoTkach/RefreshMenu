ce11d16682717d4867e656259daa9f91418edc8d
DESKTOP\PC <tkach@cwcode.ru>
2024-01-02 22:32:35 +0300

    drawer теперь хранится более абстрактно
    Took 2 minutes

--- src/main/java/tkachgeek/refreshmenu/inventory/view/View.java
+++ src/main/java/tkachgeek/refreshmenu/inventory/view/View.java
@@ -14,6 +14,7 @@ import tkachgeek.config.minilocale.Placeholders;
 import tkachgeek.refreshmenu.MenuContext;
 import tkachgeek.refreshmenu.inventory.Menu;
 import tkachgeek.refreshmenu.inventory.shape.InventoryShape;
+import tkachgeek.refreshmenu.inventory.view.drawer.AbstractDrawer;
 import tkachgeek.refreshmenu.inventory.view.drawer.ViewDrawer;
 
 @JsonTypeInfo(
@@ -31,7 +32,7 @@ public class View implements InventoryHolder {
                                                  .build();
   protected transient Menu menu = null;
   transient Behavior behavior = new Behavior();
-  transient ViewDrawer drawer;
+  transient AbstractDrawer drawer;
   transient Placeholders placeholders = new Placeholders("coder", "TkachGeek");
   transient private Inventory inventory;
   
