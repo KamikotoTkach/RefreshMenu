900fb8c04d701a9599e2b49d7e640d361bdbc8f1
Soul_KRT <soul.krt.00@gmail.com>
2026-05-06 05:44:25 +0300

    fix ExtendedView: now redraw chars on own inventory click

--- src/main/java/ru/cwcode/tkach/refreshmenu/inventory/view/ExtendedView.java
+++ src/main/java/ru/cwcode/tkach/refreshmenu/inventory/view/ExtendedView.java
@@ -4,16 +4,23 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import ru.cwcode.tkach.refreshmenu.MenuContext;
 import ru.cwcode.tkach.refreshmenu.RefreshMenu;
 import ru.cwcode.tkach.refreshmenu.inventory.ingredient.Ingredient;
 import ru.cwcode.tkach.refreshmenu.inventory.view.drawer.ExtendedViewDrawer;
+
+import java.util.Set;
 
 public class ExtendedView<T extends Ingredient> extends PagedView<T> {
   @Override
   public void onOwnInventoryClick(InventoryClickEvent event) {
     shape.findCharAtIndex(event.getRawSlot()).ifPresent(character -> {
       execute(((Player) event.getWhoClicked()), () -> {
-        behavior.execute(event, new Behavior.ClickData(character, event.getClick()));
+        boolean success = behavior.execute(event, new Behavior.ClickData(character, event.getClick()));
+        if (success) {
+          prepareForDrawing();
+          drawer.drawChars(new MenuContext(this, (Player) event.getWhoClicked()), Set.of(character));
+        }
       });
       
       super.onOwnInventoryClick(event);
