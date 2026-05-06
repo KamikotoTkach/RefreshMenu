eda73eab245852df57a3aa86ec45fc67f77ad8f8
Soul_KRT <soul.krt.00@gmail.com>
2026-05-07 00:24:48 +0300

    modify Art logic: release emptyIngredient()

--- src/main/java/ru/cwcode/tkach/refreshmenu/inventory/ingredient/ArtIngredient.java
+++ src/main/java/ru/cwcode/tkach/refreshmenu/inventory/ingredient/ArtIngredient.java
@@ -1,17 +1,22 @@
 package ru.cwcode.tkach.refreshmenu.inventory.ingredient;
 
+import org.bukkit.Material;
 import ru.cwcode.tkach.config.relocate.com.fasterxml.jackson.annotation.JsonSubTypes;
 import ru.cwcode.tkach.config.relocate.com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 @JsonTypeInfo(
-      use = JsonTypeInfo.Id.NAME,
-      property = "@type")
+   use = JsonTypeInfo.Id.NAME,
+   property = "@type")
 @JsonSubTypes({
 })
 public interface ArtIngredient extends Ingredient {
   char getChar();
-
+  
   default byte getMaxDraws() {
     return Byte.MAX_VALUE;
+  }
+  
+  default Ingredient emptyIngredient() {
+    return Ingredient.builder().type(Material.AIR).build();
   }
 }
