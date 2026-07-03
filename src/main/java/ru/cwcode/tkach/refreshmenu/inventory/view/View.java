f79edad719dc7201208c44a6f8c0dad2be5fe193
Danil Tkach <tkach@cwcode.ru>
2026-07-03 17:51:29 +0300

    fix updateInventoryTitle

--- src/main/java/ru/cwcode/tkach/refreshmenu/inventory/view/View.java
+++ src/main/java/ru/cwcode/tkach/refreshmenu/inventory/view/View.java
@@ -97,6 +97,7 @@ public abstract class View extends AbstractView {
   public void updateInventoryTitle(Player player) {
     prepareForDrawing();
     OpenedWindowService.setInventoryTitle(player, Utils.deserialize(shape.getName(), getPlaceholders(), player, false));
+    player.updateInventory();
   }
   
   public void setState(String state, String value) {
